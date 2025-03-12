ccf2eeaf7df79d12770595914d8b76bcbf26c436
esdraswite <witeesdras@gmail.com>
2025-03-12 13:05:49 +0200

    community style

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,7 +1,43 @@
+import Image from "next/image";
 import styles from "./page.module.css";
 
 const page = () => {
-  return <div className={styles.container}>Community</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.leftContent}>
+        <div className={styles.top}>
+          <h1>Créer votre communauté</h1>
+          <p>
+            Rassemblez votre communauté dans un seul endroit pour planifier des
+            événements, rester organisé et faire plus.
+          </p>
+          <button>Crée le votre</button>
+        </div>
+        <div className={styles.bottom}>
+          <h1>Créer avec un modèle</h1>
+          <div className={styles.btnGroup}>
+            <button>
+              <Image src="/graduation.svg" alt="" width={18} height={18} />
+              <span>Ecole</span>
+            </button>
+            <button>
+              <Image src="/enterprise.svg" alt="" width={18} height={18} />
+              <span>Entreprise</span>
+            </button>
+            <button>
+              <Image src="/medal.svg" alt="" width={18} height={18} />
+              <span>Vie</span>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className={styles.rightContent}>
+        <div className={styles.imgContent}>
+          <Image src="/imageCommunity.jpg" alt="" fill />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
