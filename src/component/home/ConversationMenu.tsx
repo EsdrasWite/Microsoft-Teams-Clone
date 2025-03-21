364dfbb5328a33ba5ae2892e7a2b01ffb4878758
esdraswite <witeesdras@gmail.com>
2025-03-21 13:22:26 +0200

    handle show on click

--- src/component/home/ConversationMenu.tsx
+++ src/component/home/ConversationMenu.tsx
@@ -5,6 +5,12 @@ import Image from "next/image";
 
 const ConversationMenu = () => {
   const [hoveredItem, sethoveredItem] = useState<boolean>(false);
+  const [titleExtended, settitleExtended] = useState<{
+    recent: boolean;
+    chat: boolean;
+  }>({ recent: false, chat: false });
+
+  console.log(titleExtended.recent);
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -34,18 +40,28 @@ const ConversationMenu = () => {
               alt=""
               width={15}
               height={15}
-              className={styles.ico}
+              className={`${
+                titleExtended.recent ? styles.icoExtended : styles.ico
+              }`}
             />
-            <span>Récentes</span>
+            <span
+              onClick={() =>
+                settitleExtended((prev) => ({ ...prev, recent: !prev.recent }))
+              }
+            >
+              Récentes
+            </span>
           </div>
-          <div className={styles.itemsChat}>
-            <div className={styles.itemChat}>
-              <div className={styles.avatar}>
-                <Image src="/group-1.svg" alt="" width={25} height={25} />
+          {titleExtended.recent && (
+            <div className={styles.itemsChat}>
+              <div className={styles.itemChat}>
+                <div className={styles.avatar}>
+                  <Image src="/group-1.svg" alt="" width={25} height={25} />
+                </div>
+                <span className={styles.newmsg}>Nouvelle conversation</span>
               </div>
-              <span className={styles.newmsg}>Nouvelle conversation</span>
             </div>
-          </div>
+          )}
         </div>
         <div className={styles.othersChats}>
           <div className={styles.titleContainer}>
@@ -54,30 +70,58 @@ const ConversationMenu = () => {
               alt=""
               width={15}
               height={15}
-              className={styles.ico}
+              className={`${
+                titleExtended.chat ? styles.icoExtended : styles.ico
+              }`}
             />
-            <span>Personnes que vous avez rencontrées</span>
-          </div>
-          <div className={styles.itemsContact}>
-            <div
-              className={styles.itemContact}
-              onMouseEnter={() => sethoveredItem(true)}
-              onMouseLeave={() => sethoveredItem(false)}
+            <span
+              onClick={() =>
+                settitleExtended((prev) => ({ ...prev, chat: !prev.chat }))
+              }
             >
-              <div className={styles.contactInfo}>
-                <div className={styles.avatarContact}>
-                  <span>DK</span>
+              Personnes que vous avez rencontrées
+            </span>
+          </div>
+          {titleExtended.chat && (
+            <div className={styles.itemsContact}>
+              <div
+                className={styles.itemContact}
+                onMouseEnter={() => sethoveredItem(true)}
+                onMouseLeave={() => sethoveredItem(false)}
+              >
+                <div className={styles.contactInfo}>
+                  <div className={styles.avatarContact}>
+                    <span>DK</span>
+                  </div>
+                  <div
+                    className={`${styles.names} ${
+                      hoveredItem && styles.namesActive
+                    }`}
+                  >
+                    <span>Dorcas Katoto</span>
+                    <span>Bonjour, je suis sur Teams.</span>
+                  </div>
                 </div>
-                <div
-                  className={`${styles.names} ${
-                    hoveredItem && styles.namesActive
-                  }`}
-                >
-                  <span>Dorcas Katoto</span>
-                  <span>Bonjour, je suis sur Teams.</span>
-                </div>
+                {hoveredItem && (
+                  <Image
+                    src="/more.svg"
+                    alt=""
+                    width={20}
+                    height={20}
+                    className={styles.moreIco}
+                  />
+                )}
               </div>
-              {hoveredItem && (
+              <div className={styles.itemContact}>
+                <div className={styles.contactInfo}>
+                  <div className={styles.avatarContact}>
+                    <span>EM</span>
+                  </div>
+                  <div className={styles.names}>
+                    <span>Elisée Mulumba</span>
+                    <span>Bonjour, je suis sur Teams.</span>
+                  </div>
+                </div>
                 <Image
                   src="/more.svg"
                   alt=""
@@ -85,45 +129,27 @@ const ConversationMenu = () => {
                   height={20}
                   className={styles.moreIco}
                 />
-              )}
-            </div>
-            <div className={styles.itemContact}>
-              <div className={styles.contactInfo}>
-                <div className={styles.avatarContact}>
-                  <span>EM</span>
-                </div>
-                <div className={styles.names}>
-                  <span>Elisée Mulumba</span>
-                  <span>Bonjour, je suis sur Teams.</span>
-                </div>
               </div>
-              <Image
-                src="/more.svg"
-                alt=""
-                width={20}
-                height={20}
-                className={styles.moreIco}
-              />
-            </div>
-            <div className={styles.itemContact}>
-              <div className={styles.contactInfo}>
-                <div className={styles.avatarContact}>
-                  <span>KW</span>
+              <div className={styles.itemContact}>
+                <div className={styles.contactInfo}>
+                  <div className={styles.avatarContact}>
+                    <span>KW</span>
+                  </div>
+                  <div className={styles.names}>
+                    <span>Kasereka Wite</span>
+                    <span>Bonjour, je suis sur Teams.</span>
+                  </div>
                 </div>
-                <div className={styles.names}>
-                  <span>Kasereka Wite</span>
-                  <span>Bonjour, je suis sur Teams.</span>
-                </div>
+                <Image
+                  src="/more.svg"
+                  alt=""
+                  width={20}
+                  height={20}
+                  className={styles.moreIco}
+                />
               </div>
-              <Image
-                src="/more.svg"
-                alt=""
-                width={20}
-                height={20}
-                className={styles.moreIco}
-              />
             </div>
-          </div>
+          )}
         </div>
       </div>
       <div className={styles.btnContainer}>
