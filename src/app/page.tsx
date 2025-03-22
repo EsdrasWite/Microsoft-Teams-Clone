f8a2970254c21003502de1a8810c0b52d080b211
esdraswite <witeesdras@gmail.com>
2025-03-22 12:59:19 +0200

    app root structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import ConversationMenu from "@/component/home/ConversationMenu";
+import ConversationContent from "@/component/home/ConversationContent";
+import styles from "./app.module.css";
+
 export default function Home() {
-  return <div className="">Hello</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.rootmenu}>
+        <ConversationMenu />
+      </div>
+      <div className={styles.rootcontent}>
+        <ConversationContent />
+      </div>
+    </div>
+  );
 }
