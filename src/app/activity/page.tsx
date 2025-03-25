92d51d363e0ecfbf931160a958bc14c50c311009
esdraswite <witeesdras@gmail.com>
2025-03-25 02:11:36 +0200

    page structure

--- src/app/activity/page.tsx
+++ src/app/activity/page.tsx
@@ -1,7 +1,11 @@
 import styles from "./page.module.css";
 
 const page = () => {
-  return <div className={styles.container}>Activity</div>;
+  return (
+    <div className={styles.container}>
+      <h1>Activity</h1>
+    </div>
+  );
 };
 
 export default page;
