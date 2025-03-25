92dc7d1c62fb73c0cbeb71ff007ee04574fa061e
esdraswite <witeesdras@gmail.com>
2025-03-25 02:10:24 +0200

    page structure

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,9 +1,11 @@
 import styles from "./page.module.css";
 
 const page = () => {
-  return <div className={styles.container}>
-    
-  </div>;
+  return (
+    <div className={styles.container}>
+      <h1>Calendar</h1>
+    </div>
+  );
 };
 
 export default page;
